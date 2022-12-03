43a1ca2c7eeff33cef6cdfd5417f3a9a93703d9e
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-12-03 13:57:47 +0900

    画像の紐付けと必須入力項目の追加

--- app/models/browse.rb
+++ app/models/browse.rb
@@ -1,5 +1,6 @@
 class Browse < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  attachment :car_image_id
+  attachment :car_image
+  validates :body, presence: true, length: { maximum: 1000 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :browses, dependent: :destroy
-  attachment :car_image_id
+  attachment :car_image
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 3000 }
 end
