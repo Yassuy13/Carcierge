0bd2d755ec82ef8bbd14aa5843f4d803fe24b06d
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-12-23 04:02:48 +0900

    コードミスのため内容を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
+    sessions: "users/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homes#index'
   resources :users
-  resources :posts
-  resources :browses
+  resources :posts do
+    resources :browses
+  end
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
