bdb388fd435ae5cd4999128b6d1c626cda697fe9
Yassuy13 <yassuy0413@yahoo.co.jp>
2023-03-28 14:57:10 +0900

    カテゴリー名にnullを追加

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2023_03_14_052219) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
