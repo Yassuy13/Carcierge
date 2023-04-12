c612380c0518875f0056470b023135e1435e4b1e
Yassuy13 <yassuy0413@yahoo.co.jp>
2023-04-12 18:21:07 +0900

    ログインしていない場合に投稿一覧を見たらログイン画面に遷移するように変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def index
     if params[:category_id]
